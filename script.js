1a1f0ed702b5ca7f866585cfe262024ce997e881
E1sa.g <Elisabethsenbeta@gmail.com>
2018-08-09 16:04:47 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//function (spsrmeter)
+var name = "this is my name";
+var anothername = " name" ;
+var number = "25";
+var color = "yellow";
+
+var resultbbox = document.getElementbyId('result')
+
+reslutbox.innerHTML="Something"
